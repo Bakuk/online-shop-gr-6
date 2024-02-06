393cf3975f87f7755440460f216665d35dfe2d26
Bakuk Ayas <bakuk1996@mail.ru>
2024-02-06 06:38:24 +0200

    add method minus

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -68,65 +68,23 @@ class ProductController
 
             if (isset($_POST['minus']))
             {
-                echo "Работает";
-                //header('location: /catalog' );
+                $data = UserProduct::getFilterUserProduct($userId, $productId);
+                if(!empty($data))
+                {
+                    UserProduct::updateQuantityMinus($productId, $userId);
+                    header('location: /catalog' );
+                } else {
+                    header('location: /catalog' );
+                }
             }
-        }
-        else
+        } else
         {
-            echo "Не существует такого пользователя";
+            header('location: /registrate');
         }
     }
 
-
-/*
-   public function addProduct() {
-        session_start();
-
-        if (isset($_SESSION['id'])){
-
-            if (isset($_SESSION['id'])) {
-
-                $userId = $_SESSION['id'];
-                $productId = (int)$_REQUEST['product-id'];
-                $quantity = 1; // пока 1
-
-                if (isset($_POST['plus'])){
-
-                    echo "Plus";
-                    $data = UserProduct::getFilterUserProduct($productId, $userId);
-                    if(!empty($data)){
-                        UserProduct::updateQuantityAdd($productId, $userId);
-                        header('location: /catalog' );
-                    } else {
-
-                        $this->userProduct->create($userId, $productId, $quantity);
-                        header('location: /catalog' );
-
-                    }
-                }
-
-                if (isset($_POST['minus'])){
-                    $data1 = UserProduct::getFilterUserProduct($productId, $userId);
-                    if(!empty($data1)){
-                        UserProduct::updateQuantityMinus($productId, $userId);
-                        header('location: /catalog' );
-                    } else {
-
-                        $this->userProduct->create($userId, $productId, $quantity);
-                        header('location: /catalog' );
-
-                    }
-
-                }
-
-            } else {
-                header('location: /registrate');
-            }
-        }
-    }
-*/
-    public function getCart() {
+    public function getCart()
+    {
         session_start();
         if (isset($_SESSION['id'])) {
 
